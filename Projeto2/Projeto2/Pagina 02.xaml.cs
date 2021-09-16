c3a533896d1682f92fb285ba76f07fd820689cac
AsterixBr <xandsbarbosa@gmail.com>
2021-09-16 10:58:01 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto2/Projeto2/Pagina 02.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projeto2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            StackLayout stk = new StackLayout()
+            {
+                Orientation = StackOrientation.Vertical
+            };
+            Label lb1 = new Label()
+            {
+                Text = "Curso Xamarin 2.0",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 24,
+                BackgroundColor = Color.Black,
+                Padding = new Thickness(10),
+            }
+        }
+    }
+}
